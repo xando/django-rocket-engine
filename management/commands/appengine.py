29bdf53aa66e03681a5e155d298aff52d158d49b
xando <sebastian.pawlus@gmail.com>
2012-04-02 10:13:30 +0100

    Small refactor. Removed unused parts.

--- management/commands/appengine.py
+++ management/commands/appengine.py
@@ -5,30 +5,21 @@ from ... import PROJECT_DIR, PROJECT_DIR_NAME
 from google.appengine.tools import appcfg
 
 from django.conf import settings
-from django.core.management import execute_from_command_line
 from django.core.management.base import BaseCommand
 from django.core.urlresolvers import get_callable
 
 
 PRE_UPDATE_HOOK = getattr(settings, 'APPENGINE_PRE_UPDATE',
-                              'appengine_hooks.pre_update')
+                          'appengine_hooks.pre_update')
 
 POST_UPDATE_HOOK = getattr(settings, 'APPENGINE_POST_UPDATE',
-                               'appengine_hooks.post_update')
+                           'appengine_hooks.post_update')
 
 class Command(BaseCommand):
     """Wrapper for appcfg.py with pre-update and post-update hooks"""
 
     help = 'Calls appcfg.py for the current project.'
     args = '[any options that normally would be applied to appcfg.py]'
-
-    def call_command(self, command):
-
-        if isinstance(command, basestring):
-            import ipdb; ipdb.set_trace()
-            execute_from_command_line(command.split(" "))
-        if isinstance(command, tuple) or isinstance(command, list):
-            execute_from_command_line(command)
 
     def run_from_argv(self, argv):
         if len(argv) > 2 and argv[2] == 'update':
