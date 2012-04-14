d65ce1e446a31116b892bafdfdb2ba4153ad43db
xando <sebastian.pawlus@gmail.com>
2012-04-14 13:16:07 +0100

    Changed my mind.

--- django_rocket/management/commands/appengine.py
+++ django_rocket/management/commands/appengine.py
@@ -39,80 +39,31 @@ class Command(BaseCommand):
         virtualenv = os.path.join(PROJECT_DIR, APPENGINE_VIRTUALENV)
         virtualenv_cache = os.path.join(virtualenv, 'cache')
         virtualenv_appengine_libs = os.path.join(virtualenv, 'appengine_libs')
-        virtualenv_appengine_src = os.path.join(virtualenv, 'src')
 
         appengine_libs = os.path.join(PROJECT_DIR, 'appengine_libs')
-        appengine_requirements = os.path.join(virtualenv, 'requirements.txt')
-        appengine_requirements_editable = os.path.join(virtualenv, 'requirements_editable.txt')
 
         pip_command = os.path.join(virtualenv, 'bin', 'pip')
 
         if not os.path.exists(virtualenv):
             subprocess.Popen(
-                shlex.split('virtualenv %s' % virtualenv),
+                shlex.split('virtualenv %s --distribute' % virtualenv),
             ).wait()
-
-        with open(requirements_file) as f:
-            editable = lambda x: re.match('^\s*\-e(ditable)?.+$', x) != None
-
-            file_content = f.readlines()
-
-            appengine_requirements_content="".join(
-                [line for line in file_content if not editable(line)]
-            )
-            if appengine_requirements_content:
-                open(appengine_requirements, 'w').write(
-                    appengine_requirements_content
-                )
-
-           # appengine_requirements_editable_content = "".join(
-            #     [line for line in file_content if editable(line)]
-            # )
-            # if appengine_requirements_editable_content:
-            #     open(appengine_requirements_editable, 'w').write(
-            #         appengine_requirements_editable_content
-            #     )
 
         if not os.path.exists(appengine_libs):
             os.mkdir(appengine_libs)
 
-        if os.path.exists(appengine_requirements):
-            subprocess.Popen(
-                shlex.split(
-                    "%s install --requirement=%s --download-cache=%s --target=%s"
-                    % (pip_command, appengine_requirements,
-                       virtualenv_cache, virtualenv_appengine_libs)
-                ),
-            ).wait()
+        subprocess.Popen(
+            shlex.split(
+                "%s install --requirement=%s --download-cache=%s --target=%s"
+                % (pip_command, requirements_file,
+                   virtualenv_cache, virtualenv_appengine_libs)
+            ),
+        ).wait()
 
-            for package in os.listdir(virtualenv_appengine_libs):
-                shutil.move(
-                    os.path.join(virtualenv_appengine_libs, package),
-                    os.path.join(appengine_libs, package)
-                )
-
-            os.remove(appengine_requirements)
-
-        # if os.path.exists(appengine_requirements_editable):
-        #     subprocess.Popen(
-        #         shlex.split(
-        #             "%s install --requirement=%s --download-cache=%s -d %s"
-        #             % (pip_command, appengine_requirements_editable,
-        #                virtualenv_cache, appengine_libs)
-        #         ),
-        #     ).wait()
-
-            # for package in os.listdir(virtualenv_appengine_src):
-            #     shutil.move(
-            #         os.path.join(virtualenv_appengine_src, package),
-            #         os.path.join(appengine_libs, package)
-            #     )
-
-            # os.remove(appengine_requirements_editable)
-
-        # subprocess.Popen(
-        #     shlex.split('virtualenv %s --relocatable' % virtualenv),
-        # ).wait()
+        shutil.move(
+            virtualenv_appengine_libs,
+            appengine_libs
+        )
 
     def prepare_upload(self):
         requirements_file = os.path.join(
@@ -138,10 +89,9 @@ class Command(BaseCommand):
 
     def update(self, argv):
         self.clean_upload()
-        self.prepare_upload()
 
-        return
         try:
+            self.prepare_upload()
             file_path = os.path.join(PROJECT_DIR,"_gae_module_name.py")
             hook_module = open(file_path, "w+")
             hook_module.write("PROJECT_DIR_NAME='%s'\n" % PROJECT_DIR_NAME)
