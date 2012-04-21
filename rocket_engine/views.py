7b9ddc697142eeedbf699bcb32bb2049a9a7af34
xando <sebastian.pawlus@gmail.com>
2012-04-21 18:07:26 +0100

    New storage system BlobstoreStorage and method blobstore_serve to serve blobstore files implemented.

---
+++ rocket_engine/views.py
@@ -0,0 +1,22 @@
+import urllib
+from django.utils.encoding import smart_str
+from django.http import HttpResponse
+from google.appengine.ext import blobstore
+from google.appengine.ext.blobstore import BLOB_KEY_HEADER
+
+
+def blobstore_serve(request, blobstore_key):
+
+  blobstore_key = str(urllib.unquote(blobstore_key))
+  blob_info = blobstore.BlobInfo.get(blobstore_key)
+
+  filename = getattr(blob_info, 'filename', blobstore_key)
+  content_type = getattr(blob_info, 'content_type', 'application/octet-stream')
+
+  response = HttpResponse(content_type=content_type)
+  response[BLOB_KEY_HEADER] = blobstore_key
+  response['Content-Disposition'] = smart_str(
+    u'attachment; filename=%s' % filename
+  )
+
+  return response
