e0dcef70ea1f05c9c6a0de4007d82061b134b3e9
xando <sebastian.pawlus@gmail.com>
2012-06-21 23:24:05 +0100

    Meta data updates.

--- setup.py
+++ setup.py
@@ -12,6 +12,10 @@ version = open(
     os.path.join(os.path.dirname(__file__), 'VERSION')
 ).read()
 
+license = open(
+    os.path.join(os.path.dirname(__file__), 'LICENSE')
+).read()
+
 
 setup(name='django-rocket-engine',
       version=version,
@@ -20,6 +24,8 @@ setup(name='django-rocket-engine',
       author_email='sebastian.pawlus@gmail.com',
       url='http://readthedocs.org/projects/django-rocket-engine/',
       description=short_description,
+      keywords="Django AppEngine Google CloudSQL SQL",
+      license=license,
       long_description=long_description,
       install_requires=[
           'virtualenv>=1.7.1.2',
